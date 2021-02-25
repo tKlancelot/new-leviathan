870cbc7494af7c3fb174486f7939037d2a382283
tKlancelot <princesuper42@gmail.com>
2021-02-25 19:02:27 +0100

    ok

--- js/functions/griserLi.js
+++ js/functions/griserLi.js
@@ -1,7 +1,7 @@
 export default function GriserLi(objet){
     let myLi = document.querySelectorAll("nav ul li")[objet.indiceMenu];
     // myLi.style.backgroundColor = "rgb(120 146 208)";
-    myLi.style.backgroundColor = "rgba(64, 84, 120, 0.4)";
-    myLi.style.backgroundBlendMode = "luminosity";
+    myLi.style.backgroundColor = "rgba(64, 84, 120, 0.16)";
+    myLi.style.backgroundBlendMode = "lighten";
     // console.log(myLi);
 }
