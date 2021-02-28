b46598d67efc24b7fecfb1a6bd0d47e9a58409bc
tKlancelot <princesuper42@gmail.com>
2021-02-28 14:31:18 +0100

    ok

--- supprimer.php
+++ supprimer.php
@@ -9,7 +9,7 @@
     // echo $_GET['id'];
     $req = $bdd->prepare('DELETE FROM commentaires WHERE id = ?');
     $req->execute(array($_GET['id']));
-    // header('Location: quelques-ouvrages.php');
-    header('Location: http://www.leviathan-pacifique.fr/quelques-ouvrages.php');
+    header('Location: quelques-ouvrages.php');
+    // header('Location: http://www.leviathan-pacifique.fr/quelques-ouvrages.php');
     exit();
 ?>
