10f1980f4528b24aa75dcc0e807f76f3566093b9
tKlancelot <princesuper42@gmail.com>
2021-02-28 13:18:05 +0100

    ok

--- supprimer.php
+++ supprimer.php
@@ -1,6 +1,6 @@
 <?php
-	$bdd = new PDO('mysql:host=127.0.0.1;dbname=espace-commentaire;charset=utf8','root','root');    
-	// $bdd = new PDO('mysql:host=db5001841472.hosting-data.io;dbname=dbs1514150;charset=utf8','dbu253495','!a8tAm9Rx792A8CD%');
+	// $bdd = new PDO('mysql:host=127.0.0.1;dbname=espace-commentaire;charset=utf8','root','root');    
+	$bdd = new PDO('mysql:host=db5001841472.hosting-data.io;dbname=dbs1514150;charset=utf8','dbu253495','!a8tAm9Rx792A8CD%');
 ?>
 
 
