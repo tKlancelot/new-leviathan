cdd10cfa8a38b13898a055e1b6d40fce5d519d72
tKlancelot <princesuper42@gmail.com>
2021-03-01 17:48:49 +0100

    ok

--- list-category.php
+++ list-category.php
@@ -7,7 +7,43 @@ function listCateg($bdd,string $categ){
         <div>
             <h4><?php echo $donnees['titreTechnique'];?></h4>
             <h5><?php echo $donnees['nomTechnique'];?></h5>
-            <p><?php echo $donnees['description'];?></p>
+            <table class="tableTech">
+                <tr>
+                    <td rowspan="2"><?php echo $donnees['description'];?></td>
+                    <?php 
+                        if($donnees['niveauTech'] == "easy"){
+                            echo '<td class="niveauTech centerTd niveauGreen">';
+                            echo $donnees['niveauTech'];
+                        ?></td><?php
+                        }
+                        elseif($donnees['niveauTech'] == "medium"){
+                            echo '<td class="niveauTech centerTd niveauBlue">';
+                            echo $donnees['niveauTech'];
+                        ?></td><?php
+                        }
+                        else{
+                            echo '<td class="niveauTech centerTd niveauRed">';
+                            echo $donnees['niveauTech'];
+                        ?></td><?php
+                        }
+                        ?>
+                </tr>
+                <tr>
+                    <td class="centerTd">
+                        <?php 
+                        if($donnees['niveauTech'] == "easy"){
+                            echo "<span>⭐</span>";
+                        }
+                        elseif($donnees['niveauTech'] == "medium"){
+                            echo "<span>⭐⭐</span>";
+                        }
+                        else{
+                            echo "<span>⭐⭐⭐</span>";
+                        }
+                        ?>
+                    </td>
+                </tr>
+            </table>
         </div>
         <?php
     }
